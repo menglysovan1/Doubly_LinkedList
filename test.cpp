b144e2ac68ebdeabfc53a200c4046127acfe35bb
Song Pitou <68115235+Pitou911@users.noreply.github.com>
2021-12-09 10:45:40 +0700

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -9,20 +9,7 @@ int main(){
     Double_list<int> d1;
     cout << d1.empty() << endl;
     cout << d1.size() << endl;
-    try{
-        if(d1.empty())
-            throw "underflow";
-        cout << d1.front();
-    }catch(const char *e){
-        cout << e << endl;
-    }
-    try{
-        if(d1.empty())
-            throw "underflow";
-        cout << d1.back(); 
-    }catch(const char *e){
-        cout << e << endl;
-    }
+    cout << d1.front() << " " << d1.back() << endl;
     cout << d1.head() << endl;
     cout << d1.tail() << endl;
     return 0;
